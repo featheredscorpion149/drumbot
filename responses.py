1b1e15791ee17599b250561876da54386bf6acb1
Kerri Diamond <kd333@cornell.edu>
2018-11-09 21:15:56 -0500

    OJA???

--- responses.py
+++ responses.py
@@ -13,32 +13,41 @@ class Condition(Enum):
         EXACT=1
         CONTAINS=2
         END=3
+        AND=4
 
 class Response:
-        def __init__(self, trigger, response, conditions=Condition.EXACT):
+        def __init__(self, trigger, response, conditions=Condition.CONTAINS):
                 self.triggers = trigger
                 self.response = response
                 self.conditions = conditions
         def check(self, text, textNoPunc, words):
-                for t in self.triggers:
-                        if self.conditions == Condition.EXACT:
-                                if text == t:
-                                        return True
-                        elif self.conditions == Condition.CONTAINS:
-                                if (t in textNoPunc.lower() != -1):
-                                        return True
-                        elif self.conditions == Condition.END:
-                                lower = textNoPunc.lower().strip()
-                                index = lower.find(t)
-                                if index != -1 and index == len(lower) - len(t):
-                                        return True
+                if self.conditions == Condition.AND:
+                        for t in self.triggers:
+                                if textNoPunc.lower().find(t) == -1:
+                                        return False
+                        return True
+
+                else:
+                        for t in self.triggers:
+                                if self.conditions == Condition.EXACT:
+                                        if text == t:
+                                                return True
+                                elif self.conditions == Condition.CONTAINS:
+                                        if (t in textNoPunc.lower() != -1):
+                                                return True
+                                elif self.conditions == Condition.END:
+                                        lower = textNoPunc.lower().strip()
+                                        index = lower.find(t)
+                                        if index != -1 and index == len(lower) - len(t):
+                                                return True
+
                 return False
         def reply(self):
                 return self.response
 
 responses = [
-        Response(["spicy bus ten hut"], ["FUCK YOU"]),
-        Response(["@Drumbot"], ["...I am listening"]),
+        Response(["barbeque bus ten hut"], ["FUCK YOU"], Condition.CONTAINS),
+        Response(["@Drumbot"], ["...I am listening"], Condition.EXACT),
         Response(["sniped"], ["Gottem"], Condition.CONTAINS),
 
         Response(["shh"], ["BITCH YOU GUESSED IT!", "HOO!", "...you was right"], Condition.CONTAINS),
@@ -55,10 +64,11 @@ responses = [
         Response(["penn"], ["SUCKS!"], Condition.CONTAINS),
         Response(["harvard"], ["*Hahvahd"], Condition.CONTAINS),
         Response(["aw yeah"], ["BIG BOOTY!"], Condition.CONTAINS),
-        Response(["litty titty"], ["...tiiiiitttyyy liiiiitttyyyyyy"], Condition.CONTAINS),
+        Response(["litty titty"], ["...tiiiiitttyyy liiiiitttyyyyyy..."], Condition.CONTAINS),
         Response(["its theo", "theos here"], ["Theo\'s here!", "I\'m sober!"], Condition.CONTAINS),
         Response(["trust me"], ["...truss me daddy!"], Condition.CONTAINS),
         Response(["we are percussion"], ["FUCK YOU!"], Condition.CONTAINS),
+        Response(["dont be that guy"], ["BE THAT GUY!"], Condition.CONTAINS),
         Response(["if its brown"], ["FUCK YOU!"], Condition.CONTAINS),
 
 
@@ -68,8 +78,21 @@ responses = [
         Response(["poop is"], ["BROWN!"], Condition.END),
         Response(["brown is"], ["POOP!"], Condition.END),
         Response(["go red"], ["dammit!"], Condition.END),
-        Response(["click click"], ["CLICK."], Condition.END)
+        Response(["click click"], ["CLICK."], Condition.END),
+
+        Response(["alumni status"], ["Did you mean: Alex Wong?"]),
+        Response(["barbeque sauce", "bbq sauce"], ["THAT\'S HAZING!", "@Martha_Pollack"]),
+        Response(["masturbate", "jack off", "masturbation", "jacking off"], ["That's a Band-Sanctioned event!"]),
         ]
+
+oja_responses = [
+        Response(["t-pose"], ["Please do not haze the OJA."], Condition.AND),
+        Response(["is", "hazing"], ["The OJA says: of course it is! Please turn yourself in immediately."], Condition.AND),
+        Response(["is", "a band event"], ["The OJA says: only if you're doing something bad."], Condition.AND),
+        Response(["help"], ["The OJA isn't actually here to help you, sorry."]),
+
+        Response([""], ["The OJA doesn't understand the question, but is still offended."]),
+]
 
 def get_response(message):
         text = message
@@ -79,6 +102,10 @@ def get_response(message):
 
         for r in responses:
                 if r.check(text, textNoPunc, words):
+                        return r.reply()
+
+        for r in oja_responses:
+                if text.find("@OJA") != -1 and r.check(text, textNoPunc, words):
                         return r.reply()
 
         return []
